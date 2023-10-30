60f369579630d392caf2ab3ef9e0ec7817d6cfaa
hp <hp@DESKTOP-HADGCVI>
2023-10-30 01:45:38 -0700

    Agregar archivos de proyecto.

---
+++ StatusBar_JAMB/StatusBar_JAMB.Android/statusbar/StatusBar.cs
@@ -0,0 +1,90 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Plugin.CurrentActivity;
+using StatusBar_JAMB.Droid.statusbar;
+using StatusBar_JAMB.VistaModelo;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(StatusBar))]
+
+namespace StatusBar_JAMB.Droid.statusbar
+{
+    internal class StatusBar : VMstatusbar
+    {
+        WindowManagerFlags _originalFlags;
+
+        Window GetCrurrentwindow()
+        {
+            var windows = CrossCurrentActivity.Current.Activity.Window;
+            windows.ClearFlags(WindowManagerFlags.TranslucentStatus);
+            windows.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+           return windows;
+        }
+
+        public void CambiarColor()
+        {
+            if(Build.VERSION.SdkInt>= BuildVersionCodes.M)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    var currentWindow = GetCrurrentwindow();
+                    currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LayoutFlags;
+                    currentWindow.SetStatusBarColor(Android.Graphics.Color.Rgb(18, 18, 18));
+                });
+
+                
+            }
+        }
+
+        public void MostrarStatusBar()
+        {
+            var activity = (Activity)Forms.Context;
+            var attrs = activity.Window.Attributes;
+            attrs.Flags = _originalFlags;
+            activity.Window.Attributes = attrs;
+        }
+
+        public void OcultarStatusBar()
+        {
+            var activity = (Activity)Forms.Context;
+            var attrs = activity.Window.Attributes;
+            _originalFlags = attrs.Flags;
+            attrs.Flags |= WindowManagerFlags.Fullscreen;
+            activity.Window.Attributes = attrs;
+        }
+
+        public void Transparente()
+        {
+            MostrarStatusBar();
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    var currentWindow = GetCrurrentwindow();
+                    currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LayoutFullscreen;
+                    currentWindow.SetStatusBarColor(Android.Graphics.Color.Transparent);
+                });
+
+
+            }
+        }
+
+        public void Traslucido()
+        {
+            MostrarStatusBar();
+            var activity = (Activity)Forms.Context;
+            var attrs = activity.Window.Attributes;
+            _originalFlags = attrs.Flags;
+            attrs.Flags |= WindowManagerFlags.TranslucentStatus;
+            activity.Window.Attributes = attrs;
+        }
+    }
+}
---
+++ StatusBar_JAMB/StatusBar_JAMB/VistaModelo/VMmainpage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace StatusBar_JAMB.VistaModelo
+{
+    public class VMmainpage
+    {
+        public VMmainpage()
+        {
+
+        }
+
+        public void Ocultar()
+        {
+            DependencyService.Get<VMstatusbar>().OcultarStatusBar();
+        }
+
+        public void Mostrar()
+        {
+            DependencyService.Get<VMstatusbar>().MostrarStatusBar();
+        }
+
+        public void Translucido()
+        {
+            DependencyService.Get<VMstatusbar>().Traslucido();
+        }
+
+        public void Transparente()
+        {
+            DependencyService.Get<VMstatusbar>().Transparente();
+        }
+
+        public void CambiarColor()
+        {
+            DependencyService.Get<VMstatusbar>().CambiarColor();
+        }
+
+        public ICommand OcultarCommand => new Command(Ocultar);
+
+        public ICommand MostrarCommand => new Command(Mostrar);
+
+        public ICommand TranslucidoCommand => new Command(Translucido);
+
+        public ICommand TransparenteCommand => new Command(Transparente);
+        public ICommand CambiarColorCommand => new Command(CambiarColor);
+    }
+}
